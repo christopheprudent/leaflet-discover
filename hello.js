bde029d13e7cfc59d7150a116a858e351089101d
Christophe Prudent <christophe.prudent@laposte.fr>
2018-02-14 21:10:23 +0100

    use mapbox

--- hello.js
+++
@@ -1,4 +0,0 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 10);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
