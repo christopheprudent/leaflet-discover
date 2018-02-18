005e6b525efcbafb08e815cedf94f49eae9f97b3
Christophe Prudent <christophe.prudent@laposte.fr>
2018-02-18 20:34:19 +0100

    add marker, circle and polygon

--- hello.js
+++ hello.js
@@ -1,4 +1,22 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 10);
+var mymap = L.map('mapid').setView([44.915376, -0.244534], 13);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
+
+// add marker
+var marker = L.marker([44.915376, -0.244534]).addTo(mymap);
+
+// add circle
+var circle = L.circle([44.915376, -0.3], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+// add polygon
+var polygon = L.polygon([
+    [44.915376, -0.22],
+    [44.9, -0.21],
+    [44.92, -0.19]
+]).addTo(mymap);
