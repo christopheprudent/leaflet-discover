72b35e234c7091cdd6f92aba91ba0f2c6b2e9604
Christophe Prudent <christophe.prudent@laposte.fr>
2018-04-07 20:35:24 +0200

    add a custom marker

--- hello.js
+++ hello.js
@@ -4,4 +4,9 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([43.64701, -79.39425]).addTo(map);
+var firefoxIcon = L.icon({
+    iconUrl: 'http://joshuafrazier.info/images/firefox.svg',
+    iconSize: [38, 95], // size of the icon
+    });
+
+var marker = L.marker([43.64701, -79.39425], {icon: firefoxIcon}).addTo(map);
