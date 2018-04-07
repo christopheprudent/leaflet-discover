389edb9d3228acc71f3b0dacd5892f55c2608d6a
Christophe Prudent <christophe.prudent@laposte.fr>
2018-04-07 20:33:07 +0200

    add a marker

--- hello.js
+++ hello.js
@@ -4,10 +4,4 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// add Web Map Service
-L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
-    layers: 'nexrad-n0r-900913',
-    format: 'image/png',
-    transparent: true,
-    attribution: "Weather data © 2012 IEM Nexrad"
-}).addTo(map);
+var marker = L.marker([43.64701, -79.39425]).addTo(map);
